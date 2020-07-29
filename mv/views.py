0c54fbe3188b4084337e39ed7b7ed1c5a684a1fc
JongwonYoun <jeffyoun@naver.com>
2020-07-30 04:15:18 +0900

    [feat]: mvList 화면 완성

--- mv/views.py
+++ mv/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import MusicVideo
 
 # Create your views here.
+
+
 def mvList(request):
-    return render(request, 'mvList.html')
+
+    videos = MusicVideo.objects.all()
+
+    return render(request, 'mvList.html', {'videos': videos})
+
 
 def mvDetail(request, id):
     return render(request, 'mvDetail.html')
