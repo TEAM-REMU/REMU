0350e4f7bf13e333d8c04f84467d4a719114d7c9
JongwonYoun <jeffyoun@naver.com>
2020-08-31 18:12:07 +0900

    [fix]: url 컨벤션에 맞게 수정

--- mv/urls.py
+++ mv/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 import mv.views
 
 urlpatterns = [
-    path('mvList', mv.views.mvList, name="mvList"),
-    path('mvDetail/<int:id>', mv.views.mvDetail, name="mvDetail"),
+    path('', mv.views.mvList, name="mvList"),
+    path('<int:id>', mv.views.mvDetail, name="mvDetail"),
 ]
