73fbfca77598321c0b3af71b3ad6d105c207f432
JongwonYoun <jeffyoun@naver.com>
2020-08-31 19:30:20 +0900

    [feat]: 리뷰 작성 기능 추가

--- mv/urls.py
+++ mv/urls.py
@@ -6,4 +6,5 @@ import mv.views
 urlpatterns = [
     path('', mv.views.mv_list, name="mvList"),
     path('<int:id>', mv.views.mv_detail, name="mvDetail"),
+    path('createReview/<int:mv_id>', mv.views.create_review, name="create_review"),
 ]
--- mv/views.py
+++ mv/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import MusicVideo
+from .models import MusicVideo, Review
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,9 +13,25 @@ def mv_list(request):
 
 
 def mv_detail(request, id):
-
     try:
         video = MusicVideo.objects.get(pk=id)
         return render(request, 'mvDetail.html', {'video': video})
     except MusicVideo.DoesNotExist:
         return redirect('/404')
+
+def create_review(request, mv_id):
+    try:
+        video = MusicVideo.objects.get(pk=mv_id)
+
+        review = Review()
+        # 로그인 과정 없으니 아무 유저나 가져와서 입력
+        review.author = User.objects.all().first()
+        review.video = video
+        review.text = request.POST["text"]
+        review.score = request.POST["score"]
+
+        review.save()
+        return redirect('mvDetail', mv_id)
+    except MusicVideo.DoesNotExist:
+        print("해당 뮤직비디오가 존재하지 않음")
+        return redirect('mvDetail', mv_id)
