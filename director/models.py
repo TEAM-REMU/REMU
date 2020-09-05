3700dc05dbc68ac2424bfdeab5acd372544a3dcd
2JooYeon <lewndus7749@naver.com>
2020-09-05 23:05:35 +0900

    [docs]: director 모델에 sns_link 속성 추가

--- director/models.py
+++ director/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
-
-class Director(models.Model):
+class Production(models.Model):
     # 이름
     name = models.CharField(max_length=10)
     # 이미지 주소
-    imageURL = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
     # 모델 생성 날짜
     register_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
+
+
+class Director(models.Model):
+    # 이름
+    name = models.CharField(max_length=10)
+    # 프로덕션
+    production = models.ForeignKey(Production, on_delete=models.CASCADE, blank=True, null=True)
+    # sns 주소
+    sns_link = models.TextField()
+    # 이미지 주소
+    image_url = models.CharField(max_length=100)
+    # 모델 생성 날짜
+    register_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
