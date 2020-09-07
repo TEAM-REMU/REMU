f8e0387986ebab100e33fa95c396271d1b196afe
2JooYeon <lewndus7749@naver.com>
2020-09-07 19:17:48 +0900

    [fix]: upload_date 칼럼에 null=True 추가

--- mv/models.py
+++ mv/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-from director.models import Director
+from director.models import Director, Production
 # Create your models here.
 
 
@@ -9,13 +9,15 @@ class MusicVideo(models.Model):
     # 제목
     title = models.CharField(max_length=50)
     # 감독
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, blank=True, null=True)
+    # 프로덕션
+    production = models.ForeignKey(Production, on_delete=models.CASCADE, blank=True, null=True)
     # 비디오 주소
     video_link = models.CharField(max_length=50)
     # 아티스트
     artist = models.CharField(max_length=100)
     # 뮤비 업로드 날짜
-    upload_date = models.DateTimeField()
+    upload_date = models.DateTimeField(null = True)
     # 모델 생성 날짜
     register_date = models.DateTimeField(default=timezone.now)
 
@@ -32,20 +34,10 @@ class Review(models.Model):
     # 뮤직비디오
     video = models.ForeignKey(
         MusicVideo, on_delete=models.CASCADE, related_name="reviews")
+    # 평점
+    score = models.FloatField()
     # 모델 생성 날짜
     register_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
-
-
-class Rating(models.Model):
-    # 작성자
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE)
-    # 뮤직비디오
-    video = models.ForeignKey(MusicVideo, on_delete=models.CASCADE)
-    # 평점
-    score = models.FloatField()
-    # 모델 생성 날짜
-    register_date = models.DateTimeField(default=timezone.now)
