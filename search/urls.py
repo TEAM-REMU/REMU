4a28844edc15f738cf5d7b95b7b8303a6d8bb96c
JongwonYoun <jeffyoun@naver.com>
2020-09-30 00:33:27 +0900

    [feat]: 홈이랑 search 임시 연결

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 import search.views
 
+app_name = 'search_app'
+
 urlpatterns = [
     path('', search.views.result, name="result"),
 ]
