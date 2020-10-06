e29b980f61a725118ac90bb9122d89dc6ddece1a
2JooYeon <lewndus7749@naver.com>
2020-10-07 01:02:02 +0900

    [docs]: url 간략히 수정

--- director/urls.py
+++ director/urls.py
@@ -5,7 +5,14 @@ import director.views
 
 urlpatterns = [
     #path('', director.views.director_list, name="director_list"),
-    path('director/<int:id>', director.views.director_profile, name="director_profile"),
-    path('production/<int:id>', director.views.production_profile, name="production_profile"), 
-    
+    # path('director/<int:id>', director.views.director_profile, name="director_profile"),
+    # path('production/<int:id>', director.views.production_profile, name="production_profile"), 
+    # path('director/<int:id>/popular_ordered_mv', director.views.popular_ordered_mv_of_director, name="popular_ordered_mv_of_director"),
+    # path('director/<int:id>/latest_ordered_mv', director.views.latest_ordered_mv_of_director, name="latest_ordered_mv_of_director"),
+    # path('production/<int:id>/popular_ordered_mv', director.views.popular_ordered_mv_of_production, name="popular_ordered_mv_of_production"),
+    # path('production/<int:id>/latest_ordered_mv', director.views.latest_ordered_mv_of_production, name="latest_ordered_mv_of_production"),
+    path('director/<int:id>/popular_ordered_mv', director.views.director_profile, name="popular_ordered_mv_of_director"),
+    path('director/<int:id>/latest_ordered_mv', director.views.director_profile, name="latest_ordered_mv_of_director"),
+    path('production/<int:id>/popular_ordered_mv', director.views.production_profile, name="popular_ordered_mv_of_production"),
+    path('production/<int:id>/latest_ordered_mv', director.views.production_profile, name="latest_ordered_mv_of_production"),
 ]
