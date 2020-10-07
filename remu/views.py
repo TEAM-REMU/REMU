cfdbb84f0a2587c4a8a517b59b9d9ebb36ddeff0
HYUNJo3o <sodapop47@cau.ac.kr>
2020-10-07 20:47:35 +0900

    [docs] data 오류 해결

--- remu/views.py
+++ remu/views.py
@@ -4,10 +4,9 @@ from director.models import Director
 
 # Create your views here.
 def home(request):
-    # mv1 = MusicVideo.objects.get(pk="100")
-    # mv2 = MusicVideo.objects.get(pk="200")
-    #  { 'mv1': mv1, 'mv2': mv2 }
+    #mv1 = MusicVideo.objects.get(pk="100")
     return render(request, 'home.html')
+    #{ 'mv1': mv1 })
 
 def errorPage(request):
     return render(request, '404.html')
