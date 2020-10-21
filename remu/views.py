08b54f587c6649c16cd3371088310f0318a67368
HYUNJo3o <sodapop47@cau.ac.kr>
2020-10-21 23:00:52 +0900

    [docs]: url 연결 수정

--- remu/views.py
+++ remu/views.py
@@ -7,11 +7,11 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def home(request):
     director1 = Director.objects.get(name='이래경')
-    production1 = Production.objects.get(name='룸펜스(Lumpens)')
-    production2 = Production.objects.get(name='리전드 필름(Rigend Film)')
-    director2 = Director.objects.get(name='황수아')
+    director2 = Director.objects.get(name='최용석(Yong Seok Choi)')
+    production1 = Production.objects.get(name='리전드 필름(Rigend Film)')
+    director3 = Director.objects.get(name='황수아')
     music_video = MusicVideo.objects.get(title="좋아해(bye) (Love You (bye))")
-    return render(request, 'home.html', {"director1" : director1, "production1" : production1, "production2" : production2, "director2" : director2, "music_video" : music_video})
+    return render(request, 'home.html', {"director1" : director1, "director2" : director2, "production1" : production1, "director3" : director3, "music_video" : music_video})
 
 
 def errorPage(request):
