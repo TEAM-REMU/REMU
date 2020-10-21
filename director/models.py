4d72d990814e351448e33b9b7e42b509f769dcef
HYUNJo3o <sodapop47@cau.ac.kr>
2020-10-21 22:13:45 +0900

    [docs]: url 연결

--- director/models.py
+++ director/models.py
@@ -28,5 +28,5 @@ class Director(models.Model):
     register_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return self.name + '('+str(self.pk)+')'
 
--- remu/views.py
+++ remu/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from mv.models import MusicVideo
+from mv.models import MusicVideo, Review
 from director.models import Director
 from django.shortcuts import get_object_or_404
 
 
-# Create your views here.
+# Create your views here.d（（（（
 def home(request):
-    #mv1 = MusicVideo.objects.get(pk="100")
-    #MusicVideo.objects.get_object_or_404(pk="100")
-    return render(request, 'home.html')
-    #, { 'mv1': mv1 })
+    director1 = Director.objects.get(name='이래경')
+    director2 = Director.objects.get(name='황수아')
+    music_video = MusicVideo.objects.get(title="좋아해(bye) (Love You (bye))")
+    return render(request, 'home.html', {"director1" : director1, "director2" : director2, "music_video" : music_video})
 
 
 def errorPage(request):
